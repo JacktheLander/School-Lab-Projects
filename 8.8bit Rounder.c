ecfd69f22f0ad6373d327b4afba46f8cc286c34f
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2023-11-10 11:24:16 -0800

    Update 8.8bit Rounder.c

--- 8.8bit Rounder.c
+++ 8.8bit Rounder.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main(uint8_t integer, uint8_t decimal){
+int main(uint8_t integer, uint8_t decimal){
   int16_t i = integer;
   int8_t d = decimal >> 7;
   int16_t bits = (i << 8) + d;
