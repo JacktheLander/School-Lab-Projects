26f403910ac6733f732c703928921d7751d1e91d
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2023-11-10 11:11:57 -0800

    Update 8.8bit Rounder.c

--- 8.8bit Rounder.c
+++ 8.8bit Rounder.c
@@ -3,5 +3,9 @@
 
 main(uint8_t integer, uint8_t decimal){
   int16_t i = integer;
-  int16_t bits = (i << 8) + decimal;
+  int8_t d = decimal >> 7;
+  int16_t bits = (i << 8) + d;
+
+  return bits;
+}
   
